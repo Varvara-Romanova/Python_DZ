6506c7fca8e80860b5787918020cdaceec5c3738
varvara <super.operetta2013@mail.ru>
2025-05-31 18:14:46 +0300

    Функция и номер телефона

---
+++ lesson_1_task_5.py
@@ -0,0 +1,16 @@
+# Определяем функцию
+def print_digit(num):
+    print(num)
+
+# Вызываем функцию 11 раз
+print_digit(8)
+print_digit(8)
+print_digit(0)
+print_digit(0)
+print_digit(5)
+print_digit(5)
+print_digit(5)
+print_digit(3)
+print_digit(5)
+print_digit(3)
+print_digit(5)
