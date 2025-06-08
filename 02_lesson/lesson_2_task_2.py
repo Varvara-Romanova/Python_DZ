3a72224cc74b6f9e7ca41eb017bc90d0328a437e
varvara <super.operetta2013@mail.ru>
2025-06-08 15:41:26 +0300

    Исправила ошибки

--- 02_lesson/lesson_2_task_2.py
+++ 02_lesson/lesson_2_task_2.py
@@ -4,13 +4,14 @@ def is_year_leap(year):
     else:
         return False
 
+
 # Пример вызова функции
 year_to_check = 2024
 result = is_year_leap(year_to_check)
 
 print(f"год {year_to_check}: {result}")
 
-#Пример №2
+# Пример №2
 year_to_check = 2023
 result = is_year_leap(year_to_check)
 
