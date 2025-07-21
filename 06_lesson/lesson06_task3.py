8d8c80be5faf0f4e4fca764c5c71ad2bbed3344b
varvara <super.operetta2013@mail.ru>
2025-07-21 23:19:22 +0300

    Исправила 3 задание

--- 06_lesson/lesson06_task3.py
+++ 06_lesson/lesson06_task3.py
@@ -5,28 +5,34 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Инициализация браузера
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
 try:
+    # Открытие страницы
     url = "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html "
     driver.get(url)
 
-    # Ждём загрузки всех картинок
-    WebDriverWait(driver, 15).until(
-        EC.visibility_of_all_elements_located((By.TAG_NAME, "img"))
+    # Ждём появления третьего изображения
+    third_image = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.ID, "image3"))
     )
 
-    # Получаем список изображений
-    images = driver.find_elements(By.TAG_NAME, "img")
+    # Функция для ожидания непустого src
+    def wait_for_src(element):
+        def condition(driver):
+            src = element.get_attribute("src")
+            return src is not None and len(src) > 10
+        return condition
 
-    # Выводим src третьей картинки (индекс 2)
-    if len(images) >= 3:
-        third_image_src = images[2].get_attribute("src")
-        print("src третьей картинки:")
-        print(third_image_src)
-    else:
-        print("На странице меньше трёх изображений.")
+    # Ждём, пока src будет загружен
+    WebDriverWait(driver, 10).until(wait_for_src(third_image))
+
+    # Получаем и выводим значение src
+    src_value = third_image.get_attribute("src")
+    print("src третьей картинки:")
+    print(src_value)
 
 finally:
     driver.quit()
